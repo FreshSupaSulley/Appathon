629d94ea15bda9b0e6b72cdfc414b7e12af95f2f
jason473893 <jasonyang2468@gmail.com>
2025-11-17 15:06:01 -0500

    Added UI count down clock

--- Assets/Scripts/FNAK.cs
+++ Assets/Scripts/FNAK.cs
@@ -1,16 +1,21 @@
 using System;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 
 public class FNAK : MonoBehaviour
 {
     public float totalSeconds = 30f;
-    private float startTime = Time.time;
+    private float startTime;
     // long startTime = Time.time;
+
+    public TMP_Text countdownText;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        startTime = Time.time;
     }
 
     // Update is called once per frame
@@ -18,5 +23,22 @@ public class FNAK : MonoBehaviour
     {
         // If time is over
         // unity.switchScene(gameOverScene)
+
+        float elapsed_time = Time.time - startTime;
+        float remaining_time = totalSeconds - elapsed_time;
+
+
+        int minutes = Mathf.FloorToInt(remaining_time / 60f);
+
+        int seconds = Mathf.FloorToInt(remaining_time % 60f);
+
+
+        countdownText.text = $"{minutes:00}:{seconds:00}";
+
+        if (remaining_time < 0)
+        {
+            //SceneManager.LoadScene("SceneName or Path")
+        }
+
     }
 }
