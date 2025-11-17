af0096e207b0300b18dc6ccae660ac21e8e2af0f
Erich Boschert <erich.boschert10@gmail.com>
2025-11-17 13:53:09 -0500

    game manager

---
+++ Assets/Scripts/FNAK.cs
@@ -0,0 +1,22 @@
+using System;
+using UnityEngine;
+
+public class FNAK : MonoBehaviour
+{
+    public float totalSeconds = 30f;
+    private float startTime = Time.time;
+    // long startTime = Time.time;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // If time is over
+        // unity.switchScene(gameOverScene)
+    }
+}
